ab74a0122d58910cb418cda881e30b5b390dfeaa
Mariana <mariana.iwaivk@gmail.com>
2026-01-18 11:17:10 -0300

    feat(data): add typing content for kids mode

---
+++ src/data/kidsContent.ts
@@ -0,0 +1,201 @@
+import type { KidsData } from '../types/gameTypes';
+
+export const kidsPassages: KidsData = {
+  standard: {
+    easy: [
+      { id: 'k-s-e-1', text: 'The cat is on the mat.' },
+      { id: 'k-s-e-2', text: 'I see a big blue fish.' },
+      { id: 'k-s-e-3', text: 'The red apple is sweet.' },
+      { id: 'k-s-e-4', text: 'Dogs like to run and play.' },
+      { id: 'k-s-e-5', text: 'The sun is hot and bright.' },
+      { id: 'k-s-e-6', text: 'I have a green frog.' },
+      { id: 'k-s-e-7', text: 'Look at the yellow duck.' },
+      { id: 'k-s-e-8', text: 'The bus is very big.' },
+      { id: 'k-s-e-9', text: 'My mom loves me a lot.' },
+      { id: 'k-s-e-10', text: 'Rain makes the grass grow.' },
+    ],
+    medium: [
+      { id: 'k-s-m-1', text: 'The little bird built a nest in the tall tree.' },
+      { id: 'k-s-m-2', text: 'We eat yummy popcorn when we watch a movie.' },
+      {
+        id: 'k-s-m-3',
+        text: 'The stars shine like diamonds in the night sky.',
+      },
+      { id: 'k-s-m-4', text: 'My brother and I like to build sand castles.' },
+      {
+        id: 'k-s-m-5',
+        text: 'An elephant has a very long trunk and big ears.',
+      },
+      { id: 'k-s-m-6', text: 'I can ride my bike without any help now.' },
+      { id: 'k-s-m-7', text: 'The library is a quiet place with many books.' },
+      {
+        id: 'k-s-m-8',
+        text: 'Wash your hands with soap before you eat lunch.',
+      },
+      { id: 'k-s-m-9', text: 'Bees fly from flower to flower to make honey.' },
+      { id: 'k-s-m-10', text: 'A rainbow has many colors like red and blue.' },
+    ],
+    hard: [
+      {
+        id: 'k-s-h-1',
+        text: 'Astronauts travel in a rocket ship to explore the moon and the stars.',
+      },
+      {
+        id: 'k-s-h-2',
+        text: 'The caterpillar turned into a beautiful butterfly with orange wings.',
+      },
+      {
+        id: 'k-s-h-3',
+        text: 'Please remember to use your magic words like please and thank you.',
+      },
+      {
+        id: 'k-s-h-4',
+        text: 'Dolphins are very smart animals that live in the deep blue ocean.',
+      },
+      {
+        id: 'k-s-h-5',
+        text: 'The magician pulled a fluffy white rabbit out of his black hat.',
+      },
+      {
+        id: 'k-s-h-6',
+        text: 'Planting a small seed in the dirt will help a giant sunflower grow.',
+      },
+      {
+        id: 'k-s-h-7',
+        text: 'Dinosaurs lived on our planet a long, long time before humans did.',
+      },
+      {
+        id: 'k-s-h-8',
+        text: 'Exercising every day helps your heart stay strong and your body healthy.',
+      },
+      {
+        id: 'k-s-h-9',
+        text: 'The fire truck has loud sirens and a long ladder to help people.',
+      },
+      {
+        id: 'k-s-h-10',
+        text: 'Always be kind to your friends and share your toys during playtime.',
+      },
+    ],
+  },
+  quotes: {
+    easy: [
+      { id: 'k-q-e-1', text: 'Be kind to everyone.' },
+      { id: 'k-q-e-2', text: 'You are very special.' },
+      { id: 'k-q-e-3', text: 'Keep on trying!' },
+      { id: 'k-q-e-4', text: 'Have a happy day.' },
+      { id: 'k-q-e-5', text: 'Believe in yourself.' },
+    ],
+    medium: [
+      { id: 'k-q-m-1', text: 'The more you read, the more you know.' },
+      { id: 'k-q-m-2', text: 'Why fit in when you were born to stand out?' },
+      { id: 'k-q-m-3', text: 'Today is a great day to learn something new.' },
+      { id: 'k-q-m-4', text: 'Mistakes are proof that you are trying hard.' },
+      { id: 'k-q-m-5', text: 'If you can dream it, you can do it.' },
+    ],
+    hard: [
+      {
+        id: 'k-q-h-1',
+        text: 'You have brains in your head. You have feet in your shoes.',
+      },
+      {
+        id: 'k-q-h-2',
+        text: 'No act of kindness, no matter how small, is ever wasted.',
+      },
+      {
+        id: 'k-q-h-3',
+        text: 'Winning is fun, but helping others is even better.',
+      },
+      {
+        id: 'k-q-h-4',
+        text: 'The only way to have a friend is to be one yourself.',
+      },
+      {
+        id: 'k-q-h-5',
+        text: 'It is not what happens to you, but how you react to it.',
+      },
+    ],
+  },
+  lyrics: {
+    easy: [
+      { id: 'k-l-e-1', text: 'Twinkle twinkle little star.' },
+      { id: 'k-l-e-2', text: 'Old Macdonald had a farm.' },
+      { id: 'k-l-e-3', text: 'The wheels on the bus go round.' },
+      { id: 'k-l-e-4', text: 'Row row row your boat.' },
+      { id: 'k-l-e-5', text: 'Baby shark doo doo doo doo.' },
+    ],
+    medium: [
+      {
+        id: 'k-l-m-1',
+        text: 'Let it go, let it go! Can not hold it back anymore.',
+      },
+      {
+        id: 'k-l-m-2',
+        text: 'You have got a friend in me, when the road looks rough.',
+      },
+      {
+        id: 'k-l-m-3',
+        text: 'Under the sea, darling it is better down where it is wetter.',
+      },
+      {
+        id: 'k-l-m-4',
+        text: 'A whole new world, a new fantastic point of view.',
+      },
+      { id: 'k-l-m-5', text: 'Hakuna Matata! What a wonderful phrase.' },
+    ],
+    hard: [
+      {
+        id: 'k-l-h-1',
+        text: 'I can show you the world, shining, shimmering, splendid.',
+      },
+      {
+        id: 'k-l-h-2',
+        text: 'Supercalifragilisticexpialidocious! Even though the sound of it.',
+      },
+      {
+        id: 'k-l-h-3',
+        text: 'Look for the bare necessities, the simple bare necessities.',
+      },
+      {
+        id: 'k-l-h-4',
+        text: 'The cold never bothered me anyway, I am one with the wind.',
+      },
+      {
+        id: 'k-l-h-5',
+        text: 'Remember me, though I have to say goodbye, remember me.',
+      },
+    ],
+  },
+  code: {
+    easy: [
+      { id: 'k-c-e-1', text: 'moveForward();' },
+      { id: 'k-c-e-2', text: 'turnLeft();' },
+      { id: 'k-c-e-3', text: 'jump();' },
+      { id: 'k-c-e-4', text: 'pickUpApple();' },
+      { id: 'k-c-e-5', text: 'repeat(5) { }' },
+    ],
+    medium: [
+      { id: 'k-c-m-1', text: 'if (isObstacle) { turnRight(); }' },
+      { id: 'k-c-m-2', text: 'function sayHello() { print("Hi!"); }' },
+      { id: 'k-c-m-3', text: 'for (let i = 0; i < 3; i++) { dance(); }' },
+      { id: 'k-c-m-4', text: 'setSpeed(10); startEngine();' },
+      { id: 'k-c-m-5', text: 'const myPet = "Dog"; print(myPet);' },
+    ],
+    hard: [
+      { id: 'k-c-h-1', text: 'while (hasEnergy) { run(); collectCoins(); }' },
+      {
+        id: 'k-c-h-2',
+        text: 'if (score > 100) { showLevelUp(); } else { tryAgain(); }',
+      },
+      { id: 'k-c-h-3', text: 'const fruits = ["Apple", "Banana", "Cherry"];' },
+      {
+        id: 'k-c-h-4',
+        text: 'function buildHouse(size) { addWalls(); addRoof(); }',
+      },
+      {
+        id: 'k-c-h-5',
+        text: 'document.addEventListener("click", () => { jump(); });',
+      },
+    ],
+  },
+};
