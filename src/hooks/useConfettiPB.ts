c6c54571a9d95c949e349b61136cd5e8633a053d
Mariana <mariana.iwaivk@gmail.com>
2026-01-20 05:40:54 -0300

    fix(confetti): prevent confetti firing on first PB

--- src/hooks/useConfettiPB.ts
+++ src/hooks/useConfettiPB.ts
@@ -24,47 +24,51 @@ export const useConfettiPB = ({
     if (isNewRecord) {
       localStorage.setItem(storageKey, currentValue.toString());
       setPersonalBest(currentValue);
+
       if (!isFirstTime && onNewRecord) onNewRecord();
 
-      const colors = ['#FFD700', '#C0C0C0', '#E6BE8A', '#ffffff'];
-      const fire = (particleRatio: number, opts: any) => {
-        confetti({
-          ...opts,
-          colors,
-          shapes: ['circle', 'square'],
-          ticks: 500,
-          gravity: 0.4,
-          scalar: Math.random() * 0.3 + 0.8,
-          zIndex: 1100,
-          particleCount: Math.floor(150 * particleRatio),
+      if (!isFirstTime) {
+        const colors = ['#FFD700', '#C0C0C0', '#E6BE8A', '#ffffff'];
+        const fire = (particleRatio: number, opts: any) => {
+          confetti({
+            ...opts,
+            colors,
+            shapes: ['circle', 'square'],
+            ticks: 500,
+            gravity: 0.4,
+            scalar: Math.random() * 0.3 + 0.8,
+            zIndex: 1100,
+            particleCount: Math.floor(150 * particleRatio),
+          });
+        };
+        fire(0.5, {
+          spread: 60,
+          startVelocity: 20,
+          origin: { x: 0.2, y: -0.1 },
+          angle: 280,
         });
-      };
-      fire(0.5, {
-        spread: 60,
-        startVelocity: 20,
-        origin: { x: 0.2, y: -0.1 },
-        angle: 280,
-      });
-      fire(0.5, {
-        spread: 60,
-        startVelocity: 20,
-        origin: { x: 0.8, y: -0.1 },
-        angle: 260,
-      });
-      setTimeout(() => {
-        confetti({
-          colors,
-          particleCount: 40,
-          origin: { x: 0.5, y: -0.2 },
-          gravity: 0.3,
-          startVelocity: 10,
-          ticks: 600,
-          zIndex: 1100,
+        fire(0.5, {
+          spread: 60,
+          startVelocity: 20,
+          origin: { x: 0.8, y: -0.1 },
+          angle: 260,
         });
-      }, 300);
+        setTimeout(() => {
+          confetti({
+            colors,
+            particleCount: 40,
+            origin: { x: 0.5, y: -0.2 },
+            gravity: 0.3,
+            startVelocity: 10,
+            ticks: 600,
+            zIndex: 1100,
+          });
+        }, 300);
+      }
     }
 
     if (isFirstTime && onFirstTime) onFirstTime();
+
     return { firstTime: isFirstTime, newRecord: isNewRecord };
   };
 
